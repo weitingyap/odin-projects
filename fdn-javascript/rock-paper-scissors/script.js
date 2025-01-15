2d86a60a19cef2ef1ff8f29b1bb6a4fe42e7edde
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-15 14:40:39 -0800

    Add function to get user move via prompt

--- fdn-javascript/rock-paper-scissors/script.js
+++ fdn-javascript/rock-paper-scissors/script.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+function getUserMove(){
+    // Get the user's move via prompt window
+    let move = Number(prompt("Please make a move: 1- rock, 2- paper, 3- scissors"));
+
+    // Re-prompt if invalid input received
+    while (move != 1 && move != 2 && move != 3){
+        move = Number(prompt("Invalid input. Please make a move by entering the number only: 1- rock, 2- paper, 3- scissors"));
+    }
+
+    return move;
+}
+
+let userMove = getUserMove();
