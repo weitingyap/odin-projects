8f5db9028d77aa2aa0b587b81a03c0a4a5a06463
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-16 23:45:54 -0800

    Replace console interface to button interface for player

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
-function startGameInstance(){
-    // Initiates a Scissors Paper Stone game
+// function startGameInstance(){
+//     // Initiates a Scissors Paper Stone game
 
-    const numRounds = 5;               // Set number of rounds per game
+//     const numRounds = 5;               // Set number of rounds per game
 
-    let play = 1;
+//     let play = 1;
 
-    while (play){
-        let userScore = computerScore = 0; // Initialize scores
+//     while (play){
+//         let userScore = computerScore = 0; // Initialize scores
 
-        for (let n = 0; n < numRounds; n++){
+//         for (let n = 0; n < numRounds; n++){
 
-            userMove = getUserMove();
-            computerMove = getComputerMove();
+//             userMove = getUserMove();
+//             computerMove = getComputerMove();
 
-            if (userMove === computerMove){
-                alert(`${userMove} meets ${computerMove}: Draw! Points are ${"You: " + userScore + " - Computer: " + computerScore}`);
-            } else if (
-                (userMove === 'paper' && computerMove === 'rock') ||
-                (userMove === 'rock'  && computerMove === 'scissors') || 
-                (userMove === 'scissors' && computerMove === 'paper')){
-                userScore++;
-                alert(`${userMove} beats ${computerMove}: Win! Points are ${"You: " + userScore + " - Computer: " + computerScore}`);
-            }
-            else {
-                computerScore++;
-                alert(`${userMove} loses to ${computerMove}:Loss! Points are ${"You: " + userScore + " - Computer: " + computerScore}`);
-            }
-        }
-        play = prompt(`The game is finished! Points are ${"You: " + userScore + " - Computer: " + computerScore}. 
-            Do you want to play again? Enter 1 if yes, 0 if no.`);
-        while ( play != 0 && play != 1){
-            prompt ("Invalid input. Do you want to play again? Enter 1 if yes, 0 if no.")
-        }
-    }
+//             if (userMove === computerMove){
+//                 alert(`${userMove} meets ${computerMove}: Draw! Points are ${"You: " + userScore + " - Computer: " + computerScore}`);
+//             } else if (
+//                 (userMove === 'paper' && computerMove === 'rock') ||
+//                 (userMove === 'rock'  && computerMove === 'scissors') || 
+//                 (userMove === 'scissors' && computerMove === 'paper')){
+//                 userScore++;
+//                 alert(`${userMove} beats ${computerMove}: Win! Points are ${"You: " + userScore + " - Computer: " + computerScore}`);
+//             }
+//             else {
+//                 computerScore++;
+//                 alert(`${userMove} loses to ${computerMove}:Loss! Points are ${"You: " + userScore + " - Computer: " + computerScore}`);
+//             }
+//         }
+//         play = prompt(`The game is finished! Points are ${"You: " + userScore + " - Computer: " + computerScore}. 
+//             Do you want to play again? Enter 1 if yes, 0 if no.`);
+//         while ( play != 0 && play != 1){
+//             prompt ("Invalid input. Do you want to play again? Enter 1 if yes, 0 if no.")
+//         }
+//     }
 
     
-}
+// }
 
-function getUserMove(){
-    // Get the user's move via prompt window
-    let move = (prompt("Please make a move: rock, paper, scissors")).trim().toLowerCase();
+const playerCard = document.querySelector(".player.flex-col");
+const playerButtonsArr = Array.from(document.querySelectorAll(".player > .move-btn"));
 
-    // Re-prompt if invalid input received
-    while (move != 'rock' && move != 'paper' && move != 'scissors'){
-        move = (prompt("Invalid input. Please make a move by entering the following only: rock, paper, scissors")).trim().toLowerCase();
+// register a click as a player move
+let playerMove;
+
+// use event delegation to reduce number of event listeners
+playerCard.addEventListener('click', function registerMove(event){
+    if (playerButtonsArr.includes(event.target)){
+        playerMove = event.target.innerText;
+        console.log(playerMove);
+        playerCard.removeEventListener('click', registerMove);
     }
-
-    return move;
-}
+})
 
 function getComputerMove(){
     // Randomly generate computer's move by returning a number from {1,2,3}
@@ -57,12 +60,12 @@ function getComputerMove(){
     // 1- rock, 2- paper, 3- scissors
     switch (move) {
         case 1:
-            return 'rock';
+            return 'Rock';
         case 2: 
-            return 'paper';
+            return 'Paper';
         case 3:
             return 'scissors';
     }
 }
 
-startGameInstance();
+// startGameInstance();
